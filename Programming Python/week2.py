17345ab0b7d0d0728a2c19a5df4b89586165fe77
FabioDiCeglie <fabiod@backbase.com>
2024-01-17 18:03:57 +0100

    feat: finish week2 programming in python

---
+++ Programming Python/week2.py
@@ -0,0 +1,383 @@
+# Function and variable scope
+
+# 1. Local scope
+def get_total(a, b):
+    #local variable declared inside a function
+    total = a + b
+    return total
+
+print(get_total(5, 2))
+
+# Accessing variable outside of the function:
+print(total)
+NameError: name 'total' is not defined
+
+# 2. Enclosing scope
+def get_total(a, b):
+    #enclosed variable declared inside a function
+    total = a + b
+
+    def double_it():
+        #local variable
+        double = total * 2
+        print(double)
+
+    double_it()
+    #double variable will not be accessible
+    print(double)
+
+    return total
+
+# 3. Global scope
+special = 5
+def get_total(a, b):
+    #enclosed scope variable declared inside a function
+    total = a + b
+    print(special)
+
+    def double_it():
+        #local variable
+        double = total * 2
+        print(special)
+
+    double_it()
+
+    return total
+
+# Choosing and using data structures
+
+employee_list = [(12345, "John", "Kitchen"), (12458, "Paul", "House Floor")]
+
+def get_employee(id):
+    for employee in employee_list:
+        if employee[0] == id:
+            return {"id": employee[0], "name": employee[1], "department": employee[2]}
+
+print(get_employee(12458))
+
+employee_dict = {
+    12345: {
+        "id": "12345",
+        "name": "John", 
+        "department": "Kitchen"    
+    },
+    12458: {
+        "id": "12458",
+        "name": "Paul", 
+        "department": "House Floor"    
+    }
+}
+
+def get_employee_from_dict(id):
+    return employee_dict[id]
+
+print(get_employee_from_dict(12458))
+
+## EXERCISE 
+
+menu = {
+    1: {"name": 'espresso',
+        "price": 1.99},
+    2: {"name": 'coffee', 
+        "price": 2.50},
+    3: {"name": 'cake', 
+        "price": 2.79},
+    4: {"name": 'soup', 
+        "price": 4.50},
+    5: {"name": 'sandwich',
+        "price": 4.99}
+}
+
+def calculate_subtotal(order):
+    print(order)
+    """ Calculates the subtotal of an order
+
+    [IMPLEMENT ME] 
+        1. Add up the prices of all the items in the order and return the sum
+
+    Args:
+        order: list of dicts that contain an item name and price
+
+    Returns:
+        float = The sum of the prices of the items in the order
+    """
+    print('Calculating bill subtotal...')
+    ### WRITE SOLUTION HERE
+    s = 0
+    for x in order:
+        s += x['price']
+    return s
+
+    raise NotImplementedError()
+
+def calculate_tax(subtotal):
+    """ Calculates the tax of an order
+
+    [IMPLEMENT ME] 
+        1. Multiply the subtotal by 15% and return the product rounded to two decimals.
+
+    Args:
+        subtotal: the price to get the tax of
+
+    Returns:
+        float - The tax required of a given subtotal, which is 15% rounded to two decimals.
+    """
+    print('Calculating tax from subtotal...')
+    ### WRITE SOLUTION HERE
+    tax_percentage = 0.15
+    tax_amount = round(subtotal * tax_percentage, 2)
+    return tax_amount
+
+    raise NotImplementedError()
+
+def summarize_order(order):
+    """ Summarizes the order
+
+    [IMPLEMENT ME]
+        1. Calculate the total (subtotal + tax) and store it in a variable named total (rounded to two decimals)
+        2. Store only the names of all the items in the order in a list called names
+        3. Return names and total.
+
+    Args:
+        order: list of dicts that contain an item name and price
+
+    Returns:
+        tuple of names and total. The return statement should look like 
+        
+        return names, total
+
+    """
+    print_order(order)
+    ### WRITE SOLUTION HERE
+    subtotal = calculate_subtotal(order)
+    tax = calculate_tax(subtotal)
+    total = round(subtotal + tax, 2)
+    names = []
+    for i in order:
+        names.append(i['name'])
+
+    return names, total
+
+    raise NotImplementedError()
+
+# This function is provided for you, and will print out the items in an order
+def print_order(order):
+    print('You have ordered ' + str(len(order)) + ' items')
+    items = []
+    items = [item["name"] for item in order]
+    print(items)
+    return order
+
+# This function is provided for you, and will display the menu
+def display_menu():
+    print("------- Menu -------")
+    for selection in menu:
+        print(f"{selection}. {menu[selection]['name'] : <9} | {menu[selection]['price'] : >5}")
+    print()
+
+# This function is provided for you, and will create an order by prompting the user to select menu items
+def take_order():
+    display_menu()
+    order = []
+    count = 1
+    for i in range(3):
+        item = input('Select menu item number ' + str(count) + ' (from 1 to 5): ')
+        count += 1
+        order.append(menu[int(item)])
+    return order
+
+'''
+Here are some sample function calls to help you test your implementations.
+Feel free to change, uncomment, and add these as you wish.
+'''
+def main():
+    order = take_order()
+    print_order(order)
+
+    subtotal = calculate_subtotal(order)
+    print("Subtotal for the order is: " + str(subtotal))
+
+    tax = calculate_tax(subtotal)
+    print("Tax for the order is: " + str(tax))
+
+    items, subtotal = summarize_order(order)
+    print(items, subtotal)
+
+if __name__ == "__main__":
+    main()
+
+
+## Exceptions in Python
+
+try:
+    item = items[6]
+    print(item)
+except: 
+    print("The index does not exist in the list.")
+
+
+def divide_by(a, b):
+    try:
+        return a / b
+    except ZeroDivisionError:
+        return 0
+    except Exception as e:
+        print(e, 'Something went wrong!')
+
+ans = divide_by(10,0)
+print(ans)
+
+try:
+    with open('file_does_not_exist.txt', 'r') as file:
+        print(file.read())
+except:
+    print("Unable to locate file")  
+
+
+
+#### 
+
+def read_file(file_name):
+    """ Reads in a file.
+
+    [IMPLEMENT ME]
+        1. Open and read the given file into a variable using the File read()
+           function
+        2. Print the contents of the file
+        3. Return the contents of the file
+
+    Args:
+        file_name: the name of the file to be read
+
+    Returns:
+        string: contents of the given file.
+    """
+    ### WRITE SOLUTION HERE
+    with open(file_name, "r") as file:
+        file_contents = file.read()
+
+        print(file_contents)
+    
+    return file_contents
+    
+    raise NotImplementedError()
+
+def read_file_into_list(file_name):
+    """ Reads in a file and stores each line as an element in a list
+
+    [IMPLEMENT ME]
+        1. Open the given file
+        2. Read the file line by line and append each line to a list
+        3. Return the list
+
+    Args:
+        file_name: the name of the file to be read
+
+    Returns:
+        list: a list where each element is a line in the file.
+    """
+    ### WRITE SOLUTION HERE
+    with open(file_name, "r") as file:
+        lines = file.readlines()
+    
+    return lines
+
+
+    raise NotImplementedError()
+
+def write_first_line_to_file(file_contents, output_filename):
+    """ Writes the first line of a string to a file.
+
+    [IMPLEMENT ME]
+        1. Get the first line of file_contents
+        2. Use the File write() function to write the first line into a file
+           with the name from output_filename
+
+        We determine the first line to be everything in a string before the
+        first newline ('\n') character.
+
+    Args:
+        file_contents: string to be split and written into output file
+        output_filename: the name of the file to be written to
+    """
+    ### WRITE SOLUTION HERE
+
+    # Get the first line of file_contents
+    first_line = file_contents.split('\n')[0]
+
+    # Use the File write() function to write the first line into a file
+    with open(output_filename, "w") as output_file:
+        output_file.write(first_line)
+    return
+
+    raise NotImplementedError()
+
+
+def read_even_numbered_lines(file_name):
+    """ Reads in the even numbered lines of a file
+
+    [IMPLEMENT ME]
+        1. Open and read the given file into a variable
+        2. Read the file line-by-line and add the even-numbered lines to a list
+        3. Return the list
+
+    Args:
+        file_name: the name of the file to be read
+
+    Returns:
+        list: a list of the even-numbered lines of the file
+    """
+    ### WRITE SOLUTION HERE
+    # Open and read the given file into a variable
+    with open(file_name, "r") as file:
+        # Read the file line-by-line and add the even-numbered lines to a list
+        even_lines = [line.strip() for i, line in enumerate(file) if i % 2 != 0]
+
+    # Return the list of even-numbered lines
+    return even_lines
+
+    raise NotImplementedError()
+
+def read_file_in_reverse(file_name):
+    """ Reads a file and returns a list of the lines in reverse order
+
+    [IMPLEMENT ME]
+        1. Open and read the given file into a variable
+        2. Read the file line-by-line and store the lines in a list in reverse order
+        3. Print the list
+        4. Return the list
+
+    Args:
+        file_name: the name of the file to be read
+
+    Returns:
+        list: list of the lines of the file in reverse order.
+    """
+    ### WRITE SOLUTION HERE
+    # Open and read the given file into a variable
+    with open(file_name, "r") as file:
+        # Read the file line-by-line and store the lines in a list in reverse order
+        lines = file.readlines()
+        reversed_lines = list(reversed(lines))
+
+        # Print the reversed list (optional)
+        print(reversed_lines)
+
+    # Return the list of lines in reverse order
+    return reversed_lines
+
+    raise NotImplementedError()
+
+'''
+Here are some sample commands to help you run/test your implementations.
+Feel free to uncomment/modify/add to them as you wish.
+'''
+def main():
+    file_contents = read_file("sampletext.txt")
+    print(read_file_into_list("sampletext.txt"))
+    write_first_line_to_file(file_contents, "online.txt")
+    print(read_even_numbered_lines("sampletext.txt"))
+    print(read_file_in_reverse("sampletext.txt"))
+
+if __name__ == "__main__":
+  main()

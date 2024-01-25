1802c7ba546ee7bd4c60acea4ac5f8ca7081ede4
FabioDiCeglie <fabiod@backbase.com>
2024-01-25 17:43:19 +0100

    Working with date and time functions in Python

--- Database Clients/week3/week3.py
+++ Database Clients/week3/week3.py
@@ -289,6 +289,74 @@ print(columns)
 for result in results:
     print(result)
 
+# Task 1
+sql_query = """SELECT 
+COUNT(BookingID) AS n_bookings,
+HOUR(BookingSlot) AS Hour 
+FROM Bookings
+GROUP BY Hour
+ORDER BY Hour ASC;"""
+
+# Execute the query
+cursor.execute(sql_query)
+
+# Fetch all results that satisfy the query
+results = cursor.fetchall()
+
+# Print records in the required format using for loop
+print("""Upcoming Bookings:\n""")
+for result in results:
+    print("Hour: ",result[1],"<<>>", result[0], "Booking/s")
+
+import datetime as dt
+
+# The SQL query is:
+sql_query = """SELECT 
+TableNo, 
+GuestFirstName, 
+GuestLastName, 
+BookingSlot 
+FROM Bookings 
+ORDER BY BookingSlot;"""
+
+# Execute query
+cursor.execute(sql_query)
+
+# Fetch all results that satisfy the query
+results = cursor.fetchall()
+
+# Print records in the required format
+print("The guests and their booking slots are:\n")
+for result in results:
+    time = str(result[3])
+    hour = dt.datetime.strptime(time,'%H:%M:%S').hour
+    minute = dt.datetime.strptime(time,'%H:%M:%S').minute
+    print("[Table no:]",result[0],">>",result[1],result[2], "is expected to arrive at:",
+          hour,"hrs and", minute, "mins")
+
+# The SQL query is:
+sql_query = """SELECT 
+BookingID, 
+TableNo, 
+BookingSlot, 
+ADDTIME(BookingSlot,"1:00:00") as NewTime 
+FROM Bookings
+WHERE TableNo = 12 AND BookingID = 2;"""
+
+# Execute query
+cursor.execute(sql_query)
+
+# Fetch all results that satisfy the query
+results = cursor.fetchall()
+
+# Print time change alert.
+print("Booking time change ALERT!!")
+for result in results:
+    print("Booking ID:",result[0])
+    print("Table number:",result[1])
+    print("Booked slot:",result[2])
+    print("New arrival time:",result[3])
+
 # Let's close the cursor and the connection
 if connection.is_connected():
     cursor.close()

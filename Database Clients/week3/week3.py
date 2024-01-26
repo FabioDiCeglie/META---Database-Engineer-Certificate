ed3bb777c2b05d29e06505e622910b0b24622b4a
FabioDiCeglie <fabiod@backbase.com>
2024-01-26 03:39:13 +0100

    Accessing stored procedures in a MySQL database using Python

--- Database Clients/week3/week3.py
+++ Database Clients/week3/week3.py
@@ -357,6 +357,143 @@ for result in results:
     print("Booked slot:",result[2])
     print("New arrival time:",result[3])
 
+cursor.execute("DROP PROCEDURE IF EXISTS TopSpender;")
+# Task 1
+# Stored procedure name >> TopSpender
+# Our stored procedure query is
+stored_procedure_query="""
+CREATE PROCEDURE TopSpender()
+
+BEGIN
+
+SELECT bookings.BookingID, 
+CONCAT(
+bookings.GuestFirstname,
+' ',
+bookings.GuestLastname
+) AS CustomerName,
+Orders.BillAmount 
+FROM Bookings
+INNER JOIN
+Orders ON bookings.BookingID=Orders.BookingID
+ORDER BY BillAmount DESC LIMIT 1;
+
+END
+
+"""
+
+# Execute the query
+cursor.execute(stored_procedure_query)
+
+#********************************************#
+
+# Call the stored procedure with its name
+cursor.callproc("TopSpender")
+
+# Retrieve recrods in "dataset"
+results = next( cursor.stored_results() )
+dataset = results.fetchall()
+
+# Retrieve column names using list comprehension in a 'for' loop
+for column_id in cursor.stored_results():
+    columns = [ column[0] for column in column_id.description ]
+
+# Print column names
+print(columns)
+
+# Print data
+for data in dataset:
+    print(data)
+
+cursor.execute("DROP PROCEDURE IF EXISTS NoArrival;")
+
+# Task 2
+# Stored procedure name >> NoArrival
+# Our stored procedure query is
+stored_procedure_query="""
+CREATE PROCEDURE NoArrival()
+
+BEGIN
+
+SELECT bookings.BookingID, 
+Orders.BillAmount 
+FROM Bookings
+LEFT JOIN
+Orders ON Bookings.BookingID=Orders.BookingID
+WHERE BillAmount IS NULL;
+
+END
+
+"""
+
+# Execute the query
+cursor.execute(stored_procedure_query)
+
+#********************************************#
+
+# Call the stored procedure with its name
+cursor.callproc("NoArrival")
+
+# Retrieve recrods in "dataset"
+results = next( cursor.stored_results() )
+dataset = results.fetchall()
+
+# Retrieve column names using list comprehension in a 'for' loop
+for column_id in cursor.stored_results():
+    columns = [ column[0] for column in column_id.description ]
+
+# Print column names
+print(columns)
+
+# Print data
+for data in dataset:
+    print(data)
+
+cursor.execute("DROP PROCEDURE IF EXISTS OrderStatus;")
+
+# Task 3
+# Stored procedure name >> OrderStatus
+# Our stored procedure query is
+stored_procedure_query="""
+CREATE PROCEDURE OrderStatus()
+
+BEGIN
+
+SELECT bookingID, 
+CASE
+WHEN employeeID IN (1,2,3) THEN "Order Served" 
+WHEN employeeID IN (4,5) THEN "Preparing Order" 
+ELSE "In Queue" 
+END AS Status
+FROM Bookings;
+
+END
+
+"""
+
+# Execute the query
+cursor.execute(stored_procedure_query)
+
+#********************************************#
+
+# Call the stored procedure with its name
+cursor.callproc("OrderStatus")
+
+# Retrieve recrods in "dataset"
+results = next( cursor.stored_results() )
+dataset = results.fetchall()
+
+# Retrieve column names using list comprehension in a 'for' loop
+for column_id in cursor.stored_results():
+    columns = [ column[0] for column in column_id.description ]
+
+# Print column names
+print(columns)
+
+# Print data
+for data in dataset:
+    print(data)
+
 # Let's close the cursor and the connection
 if connection.is_connected():
     cursor.close()
